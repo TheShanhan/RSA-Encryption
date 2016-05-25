6a427bde66c8ef6f9827d1ead4d5b0ea2e00d96c
TheShanhan <shahan_kevin@yahoo.com>
2016-05-25 11:38:37 -0400

    Add files via upload

--- RSA.java
+++ RSA.java
@@ -1,53 +1,79 @@
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.SecureRandom;
-import java.util.Scanner;
 
 
 public class RSA {
 	
-	private BigInteger n, d, e, phiN;
-	
+	private BigInteger p;
+	private BigInteger q;
+	private BigInteger n;
+	private BigInteger d;
+	private BigInteger e;
+	private BigInteger phiN;
 	private int bitlength = 1024;
-
+	private SecureRandom r;
 	
 	//Generate the keys
-	public RSA(int bits) {
-		bitlength = bits;
-		SecureRandom r = new SecureRandom();
-		BigInteger p = BigInteger.probablePrime(bitlength, r);
-		BigInteger q = BigInteger.probablePrime(bitlength, r);
-		BigInteger n = p.multiply(p);
-		BigInteger phiN = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
-		BigInteger e = BigInteger.probablePrime(bitlength/2, r);
-		while (phiN.gcd(e).intValue() > 1) {
-			e = e.add(new BigInteger("1"));
+	public RSA() {
+		r = new SecureRandom();
+		p = BigInteger.probablePrime(bitlength, r);
+		q = BigInteger.probablePrime(bitlength, r);
+		n = p.multiply(p);
+		phiN = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+		e = BigInteger.probablePrime(bitlength/2, r);
+		while (phiN.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phiN) < 0) {
+			e.add(BigInteger.ONE);
 		}
 		d = e.modInverse(phiN);
 	}
 	
-	//encrypt message
-	public synchronized String encrypt(String message) {
-		return (new BigInteger(message.getBytes())).modPow(e, n).toString();
+	public RSA(BigInteger e, BigInteger d, BigInteger n) {
+		this.e = e;
+		this.d = d;
+		this.n = n;
 	}
 	
-	//also encrypt message
-	public synchronized BigInteger encrypt(BigInteger message) {
-		return message.modPow(e, n);
-	}
-	
-	public static void main(String[] args){
-		RSA rsa = new RSA(1024);
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws IOException{
+		RSA rsa = new RSA();
 		
+		DataInputStream in=new DataInputStream(System.in);
+		String data;
 		System.out.println("What message would you like encrypted?");
-		String message = new Scanner(System.in).next();
+		data = in.readLine();
 		
-		//convert message into numbers
-		BigInteger cipher = new BigInteger(message.getBytes());
+		System.out.println("Message to encrypt: " + data);
+		System.out.println("Message in bytes: " + bytesToString(data.getBytes()));
 		
 		//take those numbers and encrypt them
-		BigInteger ciphertext = rsa.encrypt(cipher);
+		byte[] cipher = rsa.encrypt(data.getBytes());
+		System.out.println("Encrypted message in Bytes: " + bytesToString(cipher));
 		
-		System.out.println("Ciphertext: " + ciphertext);
+		//decrypt the numbers
+		byte[] decipher = rsa.decrypt(cipher);
+		System.out.println("Decrpyted message in Bytes: " + bytesToString(decipher));
+		System.out.println("Decrypted message " + new String(decipher));
 		
+	}
+
+	//convert the bytes into a string
+	private static String bytesToString(byte[] ciphertext) {
+		String test = "";
+			for (byte b : ciphertext) {
+				test += Byte.toString(b);
+			}
+		return test;
+	}
+	
+	//encrypt message
+	public byte[] encrypt(byte[] message) {
+		return (new BigInteger(message)).modPow(e, n).toByteArray();
+	}
+	
+	//decrypt message
+	public byte[] decrypt(byte[] message) {
+		return (new BigInteger(message)).modPow(d, n).toByteArray();
 	}
 }
